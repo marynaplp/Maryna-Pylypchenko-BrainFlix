5064afac14daff144a5acbf162cd7792b685e49e
Maryna Pylypchenko <maryna.pylypchenko@gmail.com>
2024-01-30 00:21:35 -0800

    responsive styles added

--- src/Pages/VideoUpload/VideoUploadPage.js
+++ src/Pages/VideoUpload/VideoUploadPage.js
@@ -3,21 +3,25 @@ import "./videoUpload.css"
 
 const VideoUpload=()=>{
  return(
-  <div className="video-upload">
+  <section className="video-upload">
 
   
   <main className="video-upload__main">
+  <div className="video-upload__divider"></div>
+
     <h1 className="video-upload__title">Upload Video</h1>
-    
+    <div className="video-upload__divider"></div>
+<div class="video-upload__main1">
     <form className="video-upload__form" >
       <div className="video-upload__thumbnail-section">
-        <label htmlFor="videoThumbnail">VIDEO THUMBNAIL</label>
+        <div class="video-upload__thumbnail__title">VIDEO THUMBNAIL </div>
         <div className="video-upload__thumbnail">
         </div>
       </div>
-      
+      <div className="video-upload__sections">
+
       <div className="video-upload__field">
-        <label htmlFor="videoTitle">TITLE YOUR VIDEO</label>
+        <div class="video__title">TITLE YOUR VIDEO</div>
         <input 
           type="text" 
           id="videoTitle" 
@@ -27,21 +31,27 @@ const VideoUpload=()=>{
       </div>
       
       <div className="video-upload__field">
-        <label htmlFor="videoDescription">ADD A VIDEO DESCRIPTION</label>
+     <div class="video__description">ADD A VIDEO DESCRIPTION</div>
         <textarea 
           id="videoDescription" 
           name="videoDescription" 
           placeholder="Add a description to your video"
         />
       </div>
-      
-      <div className="video-upload__buttons">
-        <button type="submit" className="video-upload__publish-btn">PUBLISH</button>
-        <button type="button" className="video-upload__cancel-btn">CANCEL</button>
       </div>
-    </form>
+      </form>
+      <div className="video-upload__divider"></div>
+
+      <div className="video-upload__buttons">
+        <button type="submit" className="video-upload__publish-btn1">PUBLISH</button>
+        <button type="button" className="video-upload__cancel-btn">CANCEL</button>
+        <button type="submit" className="video-upload__publish-btn">PUBLISH</button>
+
+      </div>
+   
+    </div>
   </main>
-</div>
+</section>
  )
 }
 export default VideoUpload;
